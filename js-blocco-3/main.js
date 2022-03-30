28d5d99a82b3cbe9f2a1439d561664dccfacee95
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-03-30 12:03:47 +0200

    pulito il codice

--- js-blocco-3/main.js
+++ js-blocco-3/main.js
@@ -4,11 +4,11 @@ Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.
 
 */
-let somma, numero_utente
-somma = 0
+let somma, numero_utente;
+somma = 0;
 
 for (let i = 0; i < 10; i++) {
     numero_utente = parseInt(prompt('Inserisci un numero'));
-    somma = parseInt(numero_utente + somma)
+    somma = somma + numero_utente;
 }
 console.log(somma);
